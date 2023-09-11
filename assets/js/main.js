11f858bb4f187e71143929ece6a64b1ba8bb4928
MarlaRey <hejmarlenehej@gmail.com>
2023-09-11 09:23:46 +0200

    ...

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//CURRENT WEATHER: ISSUE:
+function getCurrentWeather() {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=57.048820&lon=9.921747&appid=1c8284d2cba51f9f680a3c09e5602ea8`;
+  
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  
+  getCurrentWeather();
