086b5754122cd6e9c489cd35778ff81bb6a3f0c1
sachariase99 <sachariase99@hotmail.com>
2023-10-11 10:15:52 +0200

    s

--- assets/js/Modules/location.js
+++ assets/js/Modules/location.js
@@ -1,22 +1,37 @@
-const apiKey = 'd2ff6a023f11473d9533c806b6da6aba'; // OpenCage API key
+// location.js
+
+const apiKey = 'AIzaSyA_UkKyRKopA1AB4chC_rPCnWoqS3pNKuo'; // Replace with your Google Geocoding API key
 
 export async function geocodeCity(cityName) {
-    const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${cityName}&key=${apiKey}`;
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('City not found');
-        }
-        const data = await response.json();
-        if (data.results.length === 0) {
-            throw new Error('City not found');
-        }
-        const coordinates = {
-            lat: data.results[0].geometry.lat,
-            lon: data.results[0].geometry.lng
-        };
-        return coordinates;
-    } catch (error) {
-        throw error;
-    }
+   const encodedCityName = encodeURIComponent(cityName); // Encode the city name for use in the URL
+   const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedCityName}&key=${apiKey}`;
+   
+   // Log the constructed URL before making the fetch request
+   console.log('Geocoding API URL:', apiUrl);
+
+   try {
+       const response = await fetch(apiUrl);
+
+       if (!response.ok) {
+           throw new Error('City not found');
+       }
+
+       const data = await response.json();
+
+       if (data.status !== 'OK' || data.results.length === 0) {
+           // Log the response to examine its content
+           console.log('Geocoding API Response:', data);
+           throw new Error('City not found');
+       }
+
+       const location = data.results[0].geometry.location;
+       const coordinates = {
+           lat: location.lat,
+           lon: location.lng
+       };
+
+       return coordinates;
+   } catch (error) {
+       throw error;
+   }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
+// main.js
+
 import { fetchWeatherData, fetchWeatherForecast } from './Modules/weather.js';
 import { displayWeatherInfo, displayUpcomingWeather, displayUpcomingDaysWeather } from './Modules/ui.js';
 import { geocodeCity } from './Modules/location.js';
 
 const apiKey = '1c8284d2cba51f9f680a3c09e5602ea8'; // OpenWeatherMap API key
-const geocodingApiKey = 'd2ff6a023f11473d9533c806b6da6aba'; // Opencagedata API key
+const geocodingApiKey = 'AIzaSyA_UkKyRKopA1AB4chC_rPCnWoqS3pNKuo'; // Opencagedata API key
 
 // Function to get the user's current location
 function getCurrentLocation() {
   return new Promise((resolve, reject) => {
     if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
+      const options = {
+        enableHighAccuracy: true, // Try to get the most accurate location
+        timeout: 10000, // Maximum time to wait for a location (in milliseconds)
+        maximumAge: 0 // Don't use a cached location
+      };
+
+      const watchId = navigator.geolocation.watchPosition(
         position => {
           const { latitude, longitude } = position.coords;
           resolve({ lat: latitude, lon: longitude });
+          navigator.geolocation.clearWatch(watchId); // Stop watching once we have a location
         },
         error => {
           console.error('Error getting user location:', error);
           reject(error);
-        }
+        },
+        options
       );
     } else {
       console.error('Geolocation is not available in this browser.');
