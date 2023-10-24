8aa27b08384530698d43ad2da9bd2dbb45273e2d
MarlaRey <hejmarlenehej@gmail.com>
2023-10-24 10:07:23 +0200

    der var lige lidt for meget i sw

--- sw.js
+++ sw.js
@@ -7,10 +7,7 @@ if('serviceWorker' in navigator) {
 self.addEventListener('install', event => {
 	console.log('Service Worker has been installed');
 })
-// Install Service Worker
-self.addEventListener('activate', event => {
-	console.log('Service Worker has been activated');
-})
+
 // Fetch event
 self.addEventListener('fetch', event => {
 	console.log('Fetch event', event)
